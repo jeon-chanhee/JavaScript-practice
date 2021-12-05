e6ce2d05b56a95d4ebceba235a4c7ed702315ac5
jeon-chanhee <chjeon95@naver.com>
2021-12-05 21:48:24 +0900

    12/05 js 기초

---
+++ 제어문.js
@@ -0,0 +1,18 @@
+// if문 (if statement)
+
+let temperature = 0;
+
+if (temperature <= 0) {
+    console.log('물이 업니다.');
+} else {
+    console.log('물이 얼지 않습니다.');
+}
+
+// for 문
+for (let i = 1; i <= 10; i++) {
+    console.log(`${i} 코드잇 최고!`);
+}
+
+for (let i = 2; i <= 100; i*2) {
+  console.log(`${i}`);
+}
--- 추상화.js
+++ 추상화.js
@@ -24,10 +24,41 @@ introduce('코드잇',4);
 
 
 // Scope: 범위, 영역
-function myFuction() {
-    let x = 3;
-    console.log(x);
+//function myFuction() {
+//    let x = 3;
+//    console.log(x);
+//}
+//
+//let x = 3;
+//console.log(x);
+
+// 상수 (constant)
+
+const pi = 3.14; // 원주율
+const radius = 0; // 반지름
+
+
+// 실습문제
+
+// 아래에 adultTag, teenagerTag, errorTag, transferTag라는 변수들을 작성해 주세요.
+let adultTag = "삑!";
+let teenagerTag = "삑삑!";
+let transferTag = "환승입니다.";
+let errorTag = "삑삑삑!";
+
+
+function tagNotification(tagCase) {
+
+        console.log(`${tagCase}`)
 }
 
-let x = 3;
-console.log(x);
+// 아래에 tagCase파라미터를 가지는 tagNotification 함수를 작성해 주세요.
+
+
+// 테스트 코드
+tagNotification(adultTag);
+tagNotification(teenagerTag);
+tagNotification(transferTag);
+tagNotification(errorTag);
+tagNotification(adultTag);
+
